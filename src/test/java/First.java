f309a3fb1ef50b4c40c31bcc474a638f11388e99
Rafael Horvath <rbhorvath@live.com>
2022-08-10 11:23:22 -0300

    Second commit

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -2,5 +2,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("This is my first line");
         System.out.println("This is my second line");
+        System.out.println("This is my third line");
+
     }
 }
